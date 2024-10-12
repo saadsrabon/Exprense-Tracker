5b2f9804a89557568ba117e423898358eddb7ca2
Saad Srabon <saadsrabon2@gmail.com>
2024-10-13 04:14:23 +0600

    Project main files attached

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./dist/*.html", "./dist/assets/**/*.js"],
-  darkMode: "class",
-
-  variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms'),],
-}
